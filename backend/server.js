76609d4c229bd2fcfb43f070478126e002cbdf79
Shudar_shawn <shudarsanpoudel25@gmail.com>
2024-09-30 19:10:43 +0545

    Form

--- backend/server.js
+++ backend/server.js
@@ -70,6 +70,6 @@ app.post("/cmsgs", async (req, res) => {
 });
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server is running on http://0.0.0.0:${port}`);
 });
