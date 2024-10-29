4e8b10c93609f759c4d03c704dadd2e049423172
Shudar_shawn <shudarsanpoudel25@gmail.com>
2024-10-29 11:24:16 +0545

    DOne

--- src/components/Happening.jsx
+++ src/components/Happening.jsx
@@ -1,14 +1,15 @@
-import Rodri from "../assets/images/bdorrodri.jpg";
 import Opponent from "../assets/images/opponent.jpg";
+import Dor from "../assets/images/dor.jpg";
 
 function Happening() {
   // Define your items in reverse order (newest first)
   const items = [
     {
-      title: "We Wish You Rodri for Ballon d'Or",
-      image: Rodri,
+      title:
+        "Meet our first Man City Player to win Ballon D'or, Congratulation's Rodri for Ballon d'Or",
+      image: Dor,
       // Set a specific date and time for this event
-      addedTime: new Date("2024-10-28T21:00:00"), // Example date and time
+      addedTime: new Date("2024-10-29T04:00:00"), // Example date and time
     },
     {
       title: "UCL Opponents - 2024/25",
@@ -25,7 +26,7 @@ function Happening() {
   }));
 
   return (
-    <div className="bg-gradient-to-b from-blue-200 to-blue-50 p-8 sm:p-12 rounded-3xl shadow-2xl mx-4 sm:mx-auto max-w-4xl">
+    <div className="bg-gradient-to-b from-blue-200 to-blue-50 p-8 sm:p-12 rounded-3xl shadow-2xl mx-0 max-w-full">
       <h1 className="text-4xl sm:text-5xl font-bold text-gray-900 mb-10 text-center uppercase tracking-widest">
         What&apos;s Happening?
       </h1>
@@ -33,7 +34,7 @@ function Happening() {
       {itemsWithId.map((item) => (
         <div
           key={item.id}
-          className="flex flex-col items-center mb-12 p-6 bg-white rounded-2xl shadow-lg overflow-hidden transition-transform duration-300 transform hover:shadow-2xl"
+          className="flex flex-col items-center mb-12 p-6 bg-white rounded-2xl shadow-lg overflow-hidden transition-transform duration-300 transform hover:shadow-2xl w-full sm:max-w-4xl mx-auto"
         >
           <h2 className="text-2xl sm:text-3xl font-bold text-[#1b3c42] mb-10 text-center uppercase tracking-wide flex items-center">
             {item.title}
@@ -43,8 +44,7 @@ function Happening() {
             alt={item.title}
             className="w-full max-w-3xl rounded-lg shadow-lg transition-transform duration-300 hover:scale-105"
           />
-          {/* Date and Time Display below the image in a stylish box */}
-          <div className="mt-10 bg-sky-600 text-white text-lg font-semibold px-4 py-2 rounded-md shadow-md w-30 text-center">
+          <div className="mt-10 bg-sky-600 text-white text-lg font-semibold px-4 py-2 rounded-md shadow-md text-center">
             {item.addedTime.toLocaleDateString()}{" "}
             {item.addedTime.toLocaleTimeString([], {
               hour: "2-digit",
